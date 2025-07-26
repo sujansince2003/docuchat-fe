7a3062a055b8426349eebef7c18cbc208572d569
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-07-26 14:57:40 +0545

    configured to support images from sp. sources

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,18 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com"
+      },
+      {
+        protocol: "https",
+        hostname: "tailwindui.com"
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com"
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com"
       }
     ]
   }
